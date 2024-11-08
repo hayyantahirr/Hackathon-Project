858eff21859968d8248b51f79ac1210fc19c68ca
hayyantahirr <hayyantahir7@gmail.com>
2024-11-08 23:34:30 +0500

    Firebase added to project

---
+++ src/config/firebase.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3UciZC9F18CeRB0UxD1xfBvGauzKLoow",
+  authDomain: "hackathon-b60d9.firebaseapp.com",
+  projectId: "hackathon-b60d9",
+  storageBucket: "hackathon-b60d9.firebasestorage.app",
+  messagingSenderId: "162601443955",
+  appId: "1:162601443955:web:261ccf06d18e6474930006",
+  measurementId: "G-L9PD0KS4WH",
+};
+
+const app = initializeApp(firebaseConfig);
