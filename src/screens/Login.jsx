f90df50408d53e24fbbde700b91c2bd7cdd5af41
hayyantahirr <hayyantahir7@gmail.com>
2024-11-09 00:27:55 +0500

    sign in page completed

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import "../styles/Login.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Login() {
   const containerRef = useRef(null);
@@ -33,45 +34,14 @@ function Login() {
               <a href="#" className="social-icon">
                 <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" className="social-icon">
-                <i className="fab fa-twitter"></i>
-              </a>
+
               <a href="#" className="social-icon">
                 <i className="fab fa-google"></i>
-              </a>
-              <a href="#" className="social-icon">
-                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
           </form>
 
           {/* Sign Up Form */}
-          <form action="#" className="sign-up-form">
-            <h2 className="title">Sign up</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" className="btnn" value="Sign up" />
-            <p className="social-text">Or Sign up with social platforms</p>
-            <div className="social-media">
-              <a href="#" className="social-icon">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-
-              <a href="#" className="social-icon">
-                <i className="fab fa-google"></i>
-              </a>
-            </div>
-          </form>
         </div>
       </div>
 
@@ -80,36 +50,16 @@ function Login() {
         {/* Left Panel */}
         <div className="panel left-panel">
           <div className="content">
-            <h3>New here ?</h3>
+            <h3>Sign in now!</h3>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
               ex ratione. Aliquid!
             </p>
-            <button className="btnn transparentt" onClick={handleSignUpClick}>
-              Sign up
-            </button>
           </div>
           <img src="/public/log.svg" className="image" alt="Log Illustration" />
         </div>
 
         {/* Right Panel */}
-        <div className="panel right-panel">
-          <div className="content">
-            <h3>One of us ?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              laboriosam ad deleniti.
-            </p>
-            <button className="btnn transparentt" onClick={handleSignInClick}>
-              Sign in
-            </button>
-          </div>
-          <img
-            src="/public/register.svg"
-            className="image"
-            alt="Register Illustration"
-          />
-        </div>
       </div>
     </div>
   );
