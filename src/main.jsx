4a11b9ec44341979f898d02be50f6c464d56be59
hayyantahirr <hayyantahir7@gmail.com>
2024-11-08 23:28:37 +0500

    routing completed Project ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./screens/Home.jsx";
 
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
